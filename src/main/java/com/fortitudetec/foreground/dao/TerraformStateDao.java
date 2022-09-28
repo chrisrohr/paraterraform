73e061f66a7fa6968ee10a26816e04d3553ccbfd
David Singley <dsingley@vt.edu>
2022-09-28 16:55:51 -0400

    add get(id) and add(state) methods (#5)

--- src/main/java/com/fortitudetec/foreground/dao/TerraformStateDao.java
+++ src/main/java/com/fortitudetec/foreground/dao/TerraformStateDao.java
@@ -3,7 +3,11 @@ package com.fortitudetec.foreground.dao;
 import com.fortitudetec.foreground.dao.mapper.TerraformStateMapper;
 import com.fortitudetec.foreground.model.TerraformState;
 import org.jdbi.v3.sqlobject.config.RegisterRowMapper;
+import org.jdbi.v3.sqlobject.customizer.Bind;
+import org.jdbi.v3.sqlobject.customizer.BindBean;
+import org.jdbi.v3.sqlobject.statement.GetGeneratedKeys;
 import org.jdbi.v3.sqlobject.statement.SqlQuery;
+import org.jdbi.v3.sqlobject.statement.SqlUpdate;
 
 import java.util.List;
 
@@ -12,4 +16,12 @@ public interface TerraformStateDao {
 
     @SqlQuery("select * from terraform_states")
     List<TerraformState> list();
+
+    @SqlQuery("select * from terraform_states where id = :id")
+    TerraformState get(@Bind("id") Long id);
+
+    @SqlUpdate("insert into terraform_states (name, content) values (:name, :content)")
+    @GetGeneratedKeys
+    Long add(@BindBean TerraformState terraformState);
+
 }
--- src/main/java/com/fortitudetec/foreground/resource/StateResource.java
+++ src/main/java/com/fortitudetec/foreground/resource/StateResource.java
@@ -1,10 +1,9 @@
 package com.fortitudetec.foreground.resource;
 
 import com.fortitudetec.foreground.dao.TerraformStateDao;
+import com.fortitudetec.foreground.model.TerraformState;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -24,4 +23,20 @@ public class StateResource {
         return Response.ok(states).build();
     }
 
+    @GET
+    @Path("/{id}")
+    public Response get(@PathParam("id") Long id) {
+        var state = terraformStateDao.get(id);
+        if (state != null) {
+            return Response.ok(state).build();
+        }
+        return Response.status(Response.Status.NOT_FOUND).build();
+    }
+
+    @POST
+    public Response add(TerraformState state) {
+        var id = terraformStateDao.add(state);
+        state = terraformStateDao.get(id);
+        return Response.ok(state).build();
+    }
 }

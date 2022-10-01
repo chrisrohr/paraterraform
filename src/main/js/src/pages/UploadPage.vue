df0f935cfc70bbde4b1b1755037b4e77c604e73a
Selenaj2000 <61600675+Selenaj2000@users.noreply.github.com>
2022-10-01 08:15:35 -0400

    Making axios call (#22)
    * displaying terraform entries
    
    Co-authored-by: Selena Johnson <sjohnson@fortitudetec.com>

--- src/main/js/src/pages/UploadPage.vue
+++ src/main/js/src/pages/UploadPage.vue
@@ -1,5 +1,55 @@
 <template>
-  <q-page class="flex flex-center">
-    <h1>Upload page</h1>
+  <q-page>
+
+    <div class="row flex flex-center">
+      <h3>Upload Page</h3>
+    </div>
+
+    <div class="row flex flex-center">
+      <q-table
+        :rows="data"
+        :columns="columns"
+        style="width: 70%"
+      />
+    </div>
+
   </q-page>
 </template>
+
+<script>
+
+export default {
+  data() {
+    return {
+      columns: [
+        {
+          name: 'name',
+          label: 'Name',
+          align: 'left',
+          field: 'name',
+          sortable: true,
+        },
+        {
+          name: 'uploadedAt',
+          label: 'Uploaded At',
+          align: 'left',
+          field: 'uploadedAt',
+          sortable: true,
+        },
+      ],
+      data: [],
+    };
+  },
+  mounted() {
+    this.getStates();
+  },
+  methods: {
+    getStates() {
+      this.$api.get('/states')
+        .then((response) => {
+          this.data = response.data;
+        });
+    },
+  },
+};
+</script>
